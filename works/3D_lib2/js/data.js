cc75c6064e83872f753c46f652fc64417844a22e
花青素爱上叶绿素 <2912401452@qq.com>
2019-07-29 16:21:21 +0800

    3D_lib2

---
+++ works/3D_lib2/js/data.js
@@ -0,0 +1,759 @@
+
+/*
+*   type :  0 -- DIV  1--JS  2--H5  3--Css3
+*
+*
+* */
+var flyData = [
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Css3",
+        author : "张大大",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    },
+    {
+        type : "Js",
+        author : "大锤",
+        time : "2018-10-17",
+    }
+];
+
