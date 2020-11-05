8b673eff974473d69fa142c9ea54c4b0d9dee802
huaqing <huaqing@dtstack.com>
2020-11-05 11:35:55 +0800

    add smonthing

---
+++ easyvExamples/files.js
@@ -0,0 +1,25 @@
+var files = {
+	"three": [
+		"three_borderEarth",
+		"three_outLine",
+		"three_shadow",
+		"three_frameSelect",
+		"three_customTerrain",
+		"three_waterBall",
+		"three_simpleGrayPass",
+		"three_autoScaleSprite",
+		"three_cityCircle"
+	],
+	"canvas": [
+		"canvas_rose",
+		"canvas_flyLine(飞线)",
+		"canvas_pixelMouseEvent",
+		"canvas_waterBall"
+	],
+	"svg": [
+		"svg_dynamicCircle",
+	],
+	"webgl": [
+		"webgl_shadow"
+	]
+};
